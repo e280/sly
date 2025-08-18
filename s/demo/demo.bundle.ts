0a19710fde2acd891c7a02e38994321edf581dc0
Chase Moskal <chasemoskal@gmail.com>
2025-08-17 20:46:13 -0700

    cleanup: demo bundle

--- s/demo/demo.bundle.ts
+++ s/demo/demo.bundle.ts
@@ -1,9 +1,7 @@
 
-import {DemoView} from "./views/demo.js"
 import {$} from "../dom/dollar.js"
+import {DemoView} from "./views/demo.js"
 import {CounterView} from "./views/counter.js"
-import { Op } from "../ops/op.js"
-import { nap } from "@e280/stz"
 
 $.render($(".demo"), DemoView())
 
@@ -12,13 +10,4 @@ $.register({
 })
 
 console.log("🦝 sly")
-
-const op = Op.fn(async() => {
-	await nap(1000)
-	console.log("a")
-	return 123
-})
-
-op.wait.then(() => console.log("b"))
-console.log("c", await op)
 
