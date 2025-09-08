a43849782ea07b114decf083b058955d7e5939b6
Chase Moskal <chasemoskal@gmail.com>
2025-09-08 02:19:33 -0700

    tweak: earth default loader anim

--- s/ops/loaders/make-loader.ts
+++ s/ops/loaders/make-loader.ts
@@ -1,6 +1,6 @@
 
 import {Op} from "../op.js"
-import {braille} from "./parts/anims.js"
+import {earth} from "./parts/anims.js"
 import {Content} from "../../ui/types.js"
 import {ErrorDisplay} from "./parts/error-display.js"
 
@@ -9,7 +9,7 @@ export * as anims from "./parts/anims.js"
 export type Loader = <V>(op: Op<V>, ready: (value: V) => Content) => Content
 
 export function makeLoader(
-		loading: () => Content = braille,
+		loading: () => Content = earth,
 		error: (error: any) => Content = (error: any) => ErrorDisplay(error),
 	): Loader {
 
