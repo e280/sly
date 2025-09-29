492cbec9fa6ddca7ecf4d7603c6683d699c4cc7f
Chase Moskal <chasemoskal@gmail.com>
2025-09-28 23:06:32 -0700

    fix: typescript downstream with weird type exports

--- s/loaders/index.barrel.ts
+++ s/loaders/index.barrel.ts
@@ -1,5 +1,6 @@
 
 export * as anims from "./parts/anims.js"
+export type * from "./parts/anims.js"
 export * from "./parts/ascii-anim.js"
 export * from "./parts/error-display.js"
 export * from "./make.js"
--- s/loaders/index.ts
+++ s/loaders/index.ts
@@ -1,3 +1,4 @@
 
 export * as loaders from "./index.barrel.js"
+export type * from "./index.barrel.js"
 
