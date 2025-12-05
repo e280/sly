af30c7ff9726f8586813a5de4232429644ca5e2f
Chase Moskal <chasemoskal@gmail.com>
2025-12-04 23:05:40 -0800

    err: repro mounting issue

--- s/demo/views/demo.ts
+++ s/demo/views/demo.ts
@@ -4,16 +4,21 @@ import {css, html} from "lit"
 import {view} from "../../view/view.js"
 import {CounterView} from "./counter.js"
 import {LoadersView} from "./loaders.js"
+import {MountingTest} from "./mounting.js"
 import {cssReset} from "../../base/css-reset.js"
 
 export class DemoComponent extends (view.component(use => {
 	use.name("demo")
 	use.styles(cssReset, styles)
+
 	return html`
+		${MountingTest()}
+
 		${CounterView
 			.props(768, 3)
 			.children("view")
 			.render()}
+
 		${LoadersView()}
 	`
 })) {}
