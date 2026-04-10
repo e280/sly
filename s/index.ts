38c87557395763e534dd549d29e1be82c9418ccd
Chase Moskal <chasemoskal@gmail.com>
2026-04-10 16:52:04 -0700

    export: wait and spinner

--- s/index.ts
+++ s/index.ts
@@ -4,5 +4,7 @@ export * from "./loader/index.js"
 export * from "./loot/index.js"
 export * from "./op/index.js"
 export * from "./spa/index.js"
+export * from "./spinner/index.js"
 export * from "./view/index.js"
+export * from "./wait/index.js"
 
--- s/spinner/index.ts
+++ s/spinner/index.ts
@@ -1,5 +1,6 @@
 
 export * from "./spinners/earth.js"
+export * from "./spinners/spinner.js"
 export * from "./make-spinner.js"
 export * from "./types.js"
 
