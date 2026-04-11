7a3bf23e329d619ddd200c5c57640a77f31fffd9
Chase Moskal <chasemoskal@gmail.com>
2026-04-11 00:03:50 -0700

    export: moonSpinner, waveSpinner

--- s/spinner/index.ts
+++ s/spinner/index.ts
@@ -4,7 +4,9 @@ export * from "./parts/error-display.js"
 
 export * from "./spinners/dots.js"
 export * from "./spinners/earth.js"
+export * from "./spinners/moon.js"
 export * from "./spinners/spinner.js"
+export * from "./spinners/wave.js"
 
 export * from "./make-spinner.js"
 export * from "./types.js"
