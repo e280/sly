9a05a1d07ee466f0600e42ab32307af99804125b
Chase Moskal <chasemoskal@gmail.com>
2026-05-12 15:53:14 -0700

    update: stz and strata next

--- s/view/hooks/plumbing/hookscope.ts
+++ s/view/hooks/plumbing/hookscope.ts
@@ -1,11 +1,10 @@
 
-import {GMap} from "@e280/stz"
 import {Mounts} from "../use-mount.js"
 import {LightCx, ShadowCx} from "../../parts/cx.js"
 
 export class Hookscope {
 	position = 0
-	values = new GMap<number, any>()
+	values = new Map<number, any>()
 	mounts = new Mounts()
 	constructor(public cx: LightCx | ShadowCx) {}
 }
--- s/view/hooks/use-once.ts
+++ s/view/hooks/use-once.ts
@@ -1,9 +1,10 @@
 
+import {guarantee} from "@e280/stz"
 import {hooks} from "./plumbing/hooks.js"
 
 /** run the fn only one time */
 export function useOnce<Value>(fn: () => Value) {
 	const {scope, position} = hooks.increment()
-	return scope.values.guarantee(position, fn) as Value
+	return guarantee(scope.values, position, fn) as Value
 }
 
